b5a2054d13969c0e6013879f3502ef3c8fb9c9a8
Dosylia <emma.montbarbon@outlook.fr>
2024-04-23 10:33:10 +0200

    Responsive header done

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,19 +10,24 @@ function Header() {
       </NavLink>
       <nav>
         <NavLink to="/" activeclassname="active" exact="">
-          Accueil
+          <p>Accueil</p>
+          <i class="fa-solid fa-house" aria-label="Accueil"></i>
         </NavLink>
         <NavLink to="/apropos" activeclassname="active" exact="">
-          A propos
+          <p>A propos</p>
+          <i class="fa-solid fa-address-card" aria-label="A propos"></i>
         </NavLink>
         <NavLink to="/formation" activeclassname="active" exact="">
-          Formation
+          <p>Formation</p>
+          <i class="fa-solid fa-school" aria-label="Formation"></i>
         </NavLink>
         <NavLink to="/projets" activeclassname="active" exact="">
-          Projet
+          <p>Projets</p>
+          <i class="fa-solid fa-folder" aria-label="Projets"></i>
         </NavLink>
         <NavLink to="/contact" activeclassname="active" exact="">
-          Contact
+          <p>Contact</p>
+          <i class="fa-solid fa-address-book" aria-label="Contact"></i>
         </NavLink>
       </nav>
       <div className="banner banner-top-left"></div>
